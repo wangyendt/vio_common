9b68ea55a08116085cfbca625ebdbb54c70d1526
Ye Wang <905317742@qq.com>
2022-09-09 19:47:34 +0800

    增加标定需要的脚本

--- python/images_to_video_ffmpeg_format.py
+++ python/images_to_video_ffmpeg_format.py
@@ -11,7 +11,8 @@ def main():
 
 
     args = parser.parse_args()
-    assert os.path.exists(args.images_dir)
+    # print(args.images_dir)
+    # assert os.path.exists(args.images_dir)
     # assert os.path.exists(args.output_txt_path)
 
     ts_list = []
@@ -25,15 +26,15 @@ def main():
     with open(args.output_txt_path, 'w') as f:
         for i, file in enumerate(os.listdir(args.images_dir)):
             # f.write(f'file {os.path.join(args.images_dir, file)}\n')
-            if i:
-                f.write(f'duration {ts_diff[i-1]}\n')
+            # if i:
+            #     f.write(f'duration {ts_diff[i-1]}\n')
             f.write(f'file images/{file}\n')
     with open(os.path.join(os.path.dirname(args.output_txt_path), 'run_ffmpeg_images_combined.bat'), 'w') as f:
-        f.write("ffmpeg -f concat -i ffmpeg.txt movie.mp4\npause")
-    with open(os.path.join(os.path.dirname(args.output_txt_path), 'frame_timestamps.txt'), 'w') as f:
-        f.write("Frame timestamp[nanosec],Unix time[nanosec]\n")
-        for ts in ts_list:
-            f.write(f'{int(ts*1e9)},{int(ts*1e9)}\n')
+        f.write('ffmpeg -f concat -i ffmpeg.txt -vf "transpose=1" movie.mp4\npause')
+    # with open(os.path.join(os.path.dirname(args.output_txt_path), 'frame_timestamps.txt'), 'w') as f:
+    #     f.write("Frame timestamp[nanosec],Unix time[nanosec]\n")
+    #     for ts in ts_list:
+    #         f.write(f'{int(ts*1e9)},{int(ts*1e9)}\n')
 
 
 
--- python/imu_data_add_current_timestamp_and_make_frame_timestamps.py
+++ python/imu_data_add_current_timestamp_and_make_frame_timestamps.py
@@ -2,6 +2,7 @@
 
 
 import argparse
+import enum
 import imp
 import os
 import pandas as pd
@@ -12,6 +13,8 @@ import numpy as np
 def main():
     parser = argparse.ArgumentParser(description='Add a column to gyro_acc.csv collected by SlamDemo')
     parser.add_argument('imu_path', help='input imu path')
+    parser.add_argument('images_path', help='input images path')
+    parser.add_argument('frame_ts_path', help='input frame timestamp path')
 
     args = parser.parse_args()
     assert os.path.exists(args.imu_path)
@@ -25,6 +28,15 @@ def main():
     # data["timestamp"] /= 1e9
     # data = data.astype(float)
     data.to_csv(path.replace('.csv','_col_added.csv'),index=None)
+    if os.path.exists(args.frame_ts_path):
+        os.remove(args.frame_ts_path)
+    with open(args.frame_ts_path, 'w') as f:
+        f.write('Frame timestamp[nanosec],Unix time[nanosec]\n')
+        for i, file in enumerate(os.listdir(args.images_path)):
+            save_time = int(file[:file.index('.')]) - data.loc[0, "timestamp"] + data.loc[0, "current_timestamp"]
+            # print(f"{file[:file.index('.')]},{save_time}\n",save_time)
+            f.write(f"{file[:file.index('.')]},{save_time}\n")
+
 
 
 
